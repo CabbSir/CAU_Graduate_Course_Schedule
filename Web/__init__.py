6637fe7555dec40b7ecc84a381f7fe7e2bb041bd
cabbsir <cabbsir@debian.cabbsir.com>
2021-10-12 15:39:52 +0800

    自定义404页面

--- Web/__init__.py
+++ Web/__init__.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from .config import config
 from . import models, routes
 
 
-def create_app(config_name = 'default'):
+def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     models.init_app(app)
     routes.init_app(app)
+
+    @app.errorhandler(404)
+    def handle_404_error(e):
+        return render_template("404.html"), 404
 
     return app
