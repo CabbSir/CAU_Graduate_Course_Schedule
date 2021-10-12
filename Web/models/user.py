8580aa934dfe4a992467dca57d1755a8a8235feb
cabbsir <cabbsir@debian.cabbsir.com>
2021-10-12 16:23:41 +0800

    建立user表

---
+++ Web/models/user.py
@@ -0,0 +1,14 @@
+from time import time
+
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(255), nullable=False)
+    j_name = db.Column(db.String(255), nullable=True)
+    j_passwd = db.Column(db.String(255), nullable=True)
+    last_login_ip = db.Column(db.String(50), nullable=True)
+    last_login_time = db.Column(db.String(10), nullable=True)
+    create_time = db.Column(db.String(10), nullable=False, default=time())
+    modify_time = db.Column(db.String(10), nullable=False, default=time())
