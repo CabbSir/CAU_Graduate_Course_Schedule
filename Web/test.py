fdbae2af6c2ce908852a7de3f17b198902d1ec2f
cabbsir <cabbsir@gmail.com>
2021-10-14 00:33:18 +0800

    获取课表的测试文件更新

--- Web/test.py
+++ Web/test.py
@@ -1,18 +1,23 @@
-import base64
-
 import requests
+from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-    name = "SY20213213219"
-    passwd = "306186753@qq"
-    captcha = ""
-
-    url = "http://gradinfo.cau.edu.cn/j_acegi_security_check"
-    data = {
-        'j_username': name,
-        'j_password': passwd,
-        'j_captcha': captcha,
-        'groupId': ''
+    url = 'http://gradinfo.cau.edu.cn/studentschedule/showStudentSchedule.do?groupId=&moduleId=20101'
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh,zh-CN;q=0.9',
+        'Cookie': 'JSESSIONID=07DB6B25516F0D3EA565429FFC88DD31.TA2',
+        'Host': 'gradinfo.cau.edu.cn',
+        'Referer': 'http://gradinfo.cau.edu.cn/index.do',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
     }
-    ret = requests.post(url, data)
-    print(ret.content)
+    ret = requests.get(url, headers = headers)
+    soup = BeautifulSoup(ret.text, "html.parser")
+    table = soup.body.table
+    trs = table.find_all('tr')
+    for tr in trs:
+        for td in tr.find_all('td'):
+            print(td.get_text(strip=True), end='')
+            print(" ")
