f69fc8355ac08abbf982935647b0a1439c9f391d
cabbsir <cabbsir@debian.cabbsir.com>
2021-10-14 08:39:40 +0800

    udpate database

--- Web/models/__init__.py
+++ Web/models/__init__.py
@@ -6,12 +6,13 @@ db = SQLAlchemy()
 
 
 def init_db():
+    from . import course, user, detail, course_detail_relation, user_course_relation
     """Clear existing data and create new tables."""
     db.drop_all()
     db.create_all()
 
 
-@click.command("init-db") # python3 -m flask init-db
+@click.command("init-db")  # python3 -m flask init-db
 @with_appcontext
 def init_db_command():
     """Clear existing data and create new tables."""
--- Web/models/user_course_relation.py
+++ Web/models/user_course_relation.py
@@ -3,5 +3,6 @@ from . import db
 
 class UserCourseRelation(db.Model):
     __tablename__ = 'tb_user_course_relation'
+    id = db.Column(db.Integer, nullable = False, primary_key = True) # orm必须创建主键
     user_id = db.Column(db.Integer, nullable = False)
     course_id = db.Column(db.Integer, nullable = False)
