5e7684814d22ed00248a478c5059083b5e8a64ed
cabbsir <cabbsir@gmail.com>
2021-10-14 19:27:54 +0800

    update model structure

--- Web/models/course.py
+++ Web/models/course.py
@@ -9,7 +9,7 @@ class Course(db.Model):
     no = db.Column(db.String(20), nullable = False)
     name = db.Column(db.String(50), nullable = False)
     class_no = db.Column(db.Integer, nullable = False)
-    point = db.Column(db.Numeric, nullable = False)
+    point = db.Column(db.Numeric(10, 1), nullable = False)
     week_start = db.Column(db.SmallInteger, nullable = False)
     week_end = db.Column(db.SmallInteger, nullable = False)
     create_time = db.Column(db.Integer, nullable = False, default = int(time.time()))
