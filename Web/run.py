9fdc2ae51e05343e55c9e80f628275100720564e
cabbsir <cabbsir@gmail.com>
2021-10-18 22:47:03 +0800

    提交uwsgi所需run.py

--- Web/run.py
+++ Web/run.py
@@ -1,5 +1,24 @@
-from . import create_app
+from flask import Flask, render_template
+from .config import config
+from . import models, routes
+
+
+def create_app(config_name = 'default'):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    app.debug = False
+    models.init_app(app)
+    routes.init_app(app)
+
+    @app.errorhandler(404)
+    def handle_404_error(e):
+        return render_template("404.html"), 404
+
+    return app
+
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host = '0.0.0.0', port=5000)
+    app.run(host = '0.0.0.0', port = 5000)
